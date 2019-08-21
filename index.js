c6aff2b87627ded1e1aa3b7ecbf26fe521983d34
Uncle Sam <djiasara@gmail.com>
2019-08-21 04:18:32 -0400

    invoking speech recognition api

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+//setting up recognition interface regardless of the browser: 
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+const recognition = new SpeechRecognition();
+const icon = document.querySelector('i.fa.fa-microphone')
+let paragraph = document.createElement('p');
+let container = document.querySelector('.text-box');
+container.appendChild(paragraph);
+const sound = document.querySelector('.sound');
+
+icon.addEventListener('click', () => {
+    sound.play();
+    dictate();
+  });
+  const dictate = () => {
+    recognition.start();
+    recognition.onresult = (event) => {
+        const speechToText = event.results[0][0].transcript;
+        
+        paragraph.textContent = speechToText;
+      }
+  }
+  
